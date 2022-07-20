5f31d18e0f53a7bd91c11b31a6586469d748ffee
Lateef Quadri Olayinka <lateef9816@gmail.com>
2022-07-20 18:32:56 +0100

    feat: integrated endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,42 @@ import "./App.css";
 
 import { Box, Flex } from "@chakra-ui/react";
 import TableComponent from "./components/table";
+import { useEffect, useState } from "react";
 
-import { data } from "./data";
+// import { data } from "./data";
 
 function App() {
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState("");
-  // const [dataa, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [data, setData] = useState([]);
 
-  // useEffect(() => {
-  //   setLoading(true);
-  //   const fetchData = () => {
-  //     fetch("https://tender-mclean-00a2bd.netlify.app/web/movies.json")
-  //       .then((response) => {
-  //         // setData(response);
-  //         console.log({ response });
-  //       })
-  //       .catch((error) => {
-  //         console.log({ error });
-  //         setError(error.message);
-  //       })
-  //       .finally(() => setLoading(false));
-  //   };
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = () => {
+      fetch("https://tender-mclean-00a2bd.netlify.app/web/movies.json")
+        .then((response) => response.json())
+        .then((responseJson) => {
+          setData(responseJson);
+        })
+        .catch((error) => {
+          setError(error.message);
+        })
+        .finally(() => setLoading(false));
+    };
 
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
 
   return (
     <Flex w="100%" h="100vh" alignItems="center" justifyContent="center">
       <Box minW="90%" w="1000px" h="90%">
-        <TableComponent data={data} />
+        {loading ? (
+          <Box>Loading</Box>
+        ) : error ? (
+          <Box>{error}</Box>
+        ) : (
+          <TableComponent data={data} />
+        )}
       </Box>
     </Flex>
   );
